c04edcb99b36ce0181c5b69ec9bb17540924f602
toven <ttw130@gmail.com>
2021-08-22 22:48:53 +0800

    完善 http 错误处理

--- http/error.go
+++ http/error.go
@@ -1,11 +1,16 @@
 package http
 
+import (
+	"code.simpleel.com/simpleel/kit/errors"
+	"code.simpleel.com/simpleel/kit/http/errcode"
+)
+
 // NewInternalServerError 返回一个服务器内部错误
 func NewInternalServerError(msg string) error {
-	return nil
+	return errors.NewCodeMsg(errcode.ErrServerCode, msg)
 }
 
 // NewOprationFaildError 返回一个操作失败的错误
-func NewOprationFaildError() error {
-	return nil
+func NewOprationFaildError(msg string) error {
+	return errors.NewCodeMsg(errcode.FailedCode, msg)
 }
