df2ace4f2c8e41cfc1c82090dfcba5c84ae25d2b
toven <ttw130@gmail.com>
2021-08-24 22:49:38 +0800

    修改错误参数为可选参数

--- http/error.go
+++ http/error.go
@@ -6,16 +6,16 @@ import (
 )
 
 // NewInternalServerError 返回一个服务器内部错误
-func NewInternalServerError(msg string) error {
-	return errors.NewCodeMsg(errcode.ErrServerCode, msg)
+func NewInternalServerError(msg ...string) error {
+	return errors.NewCodeMsg(errcode.ErrServerCode, msg...)
 }
 
 // NewOprationFaildError 返回一个操作失败的错误
-func NewOprationFaildError(msg string) error {
-	return errors.NewCodeMsg(errcode.FailedCode, msg)
+func NewOprationFaildError(msg ...string) error {
+	return errors.NewCodeMsg(errcode.FailedCode, msg...)
 }
 
 // NewAccessDenyError 返回一个越权错误
-func NewAccessDenyError(msg string) error {
-	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, msg)
+func NewAccessDenyError(msg ...string) error {
+	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, msg...)
 }
