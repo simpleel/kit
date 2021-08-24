46945a24609c361caf22220ac32bb06e1cc43c69
toven <ttw130@gmail.com>
2021-08-24 22:50:54 +0800

    ...

--- http/error.go
+++ http/error.go
@@ -7,15 +7,15 @@ import (
 
 // NewInternalServerError 返回一个服务器内部错误
 func NewInternalServerError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.ErrServerCode, msg...)
+	return errors.NewCodeMsg(errcode.ErrServerCode, msg)
 }
 
 // NewOprationFaildError 返回一个操作失败的错误
 func NewOprationFaildError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.FailedCode, msg...)
+	return errors.NewCodeMsg(errcode.FailedCode, msg)
 }
 
 // NewAccessDenyError 返回一个越权错误
 func NewAccessDenyError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, msg...)
+	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, msg)
 }
