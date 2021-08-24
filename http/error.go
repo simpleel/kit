760d38d3a9847a4746aa012ac7b766c0b8586d69
toven <ttw130@gmail.com>
2021-08-24 23:10:02 +0800

    fixed errors

--- http/error.go
+++ http/error.go
@@ -1,21 +1,35 @@
 package http
 
 import (
+	"strings"
+
 	"code.simpleel.com/simpleel/kit/errors"
 	"code.simpleel.com/simpleel/kit/http/errcode"
 )
 
 // NewInternalServerError 返回一个服务器内部错误
 func NewInternalServerError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.ErrServerCode, msg)
+	s := combineMsg(msg...)
+	return errors.NewCodeMsg(errcode.ErrServerCode, s)
 }
 
 // NewOprationFaildError 返回一个操作失败的错误
 func NewOprationFaildError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.FailedCode, msg)
+	s := combineMsg(msg...)
+	return errors.NewCodeMsg(errcode.FailedCode, s)
 }
 
 // NewAccessDenyError 返回一个越权错误
 func NewAccessDenyError(msg ...string) error {
-	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, msg)
+	s := combineMsg(msg...)
+	return errors.NewCodeMsg(errcode.ErrAccessDenyCode, s)
+}
+
+func combineMsg(msg ...string) string {
+	if len(msg) > 1 {
+		return strings.Join(msg, " ")
+	} else if len(msg) == 1 {
+		return msg[0]
+	}
+	return ""
 }
---
+++ http/http_test.go
@@ -0,0 +1,11 @@
+package http_test
+
+import (
+	"testing"
+
+	mhttp "code.simpleel.com/simpleel/kit/http"
+)
+
+func TestNewCodeMsg(t *testing.T) {
+	mhttp.NewAccessDenyError()
+}
