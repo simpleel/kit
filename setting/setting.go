1dfe12ca828944b7c7ed0e23a184fbc9041e2abb
toven <toven@advan.onaliyun.com>
2021-08-25 15:22:20 +0800

    抽象配置类

---
+++ setting/setting.go
@@ -0,0 +1,22 @@
+package setting
+
+import "encoding/json"
+
+// Settings 表示一个应用配置的实体对象
+type Settings interface {
+	Name()
+}
+
+// Unmarshal 将 JSON 字符串转换为实体对象，in 必须是 JSON 格式的字符串。
+func Unmarshal(settings Settings, in string) error {
+	return json.Unmarshal([]byte(in), settings)
+}
+
+// Marshal 将实体对象转换为 JSON 字符串
+func Marshal(settings Settings) string {
+	b, err := json.Marshal(settings)
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
