316c05ecc8b5c9e39ca87ac11aadd0062e90cb82
toven <ttw130@gmail.com>
2021-09-10 15:48:44 +0800

    增加注释

--- sql/query/condition.go
+++ sql/query/condition.go
@@ -10,15 +10,18 @@ type condition struct {
 	values []interface{}
 }
 
+// NewCondition 返回 SQL 条件构造对象
 func NewCondition() *condition {
 	return &condition{}
 }
 
+// Add 添加一个查询参数和值
 func (c *condition) Add(param string, value interface{}) {
 	c.params = append(c.params, param)
 	c.values = append(c.values, value)
 }
 
+// SQLParams 返回 SQL 查询参数字符串
 func (c *condition) SQLParams() string {
 	if len(c.params) == 0 {
 		return ""
@@ -27,6 +30,7 @@ func (c *condition) SQLParams() string {
 	return strings.Join(c.params, " and ")
 }
 
+// SQLWhere 返回有 where 字符串的查询参数字符串
 func (c *condition) SQLWhere() string {
 	var s = c.SQLParams()
 	if len(s) == 0 {
@@ -35,6 +39,7 @@ func (c *condition) SQLWhere() string {
 	return fmt.Sprintf(" where %s", s)
 }
 
+// SQLValues 返回 SQL 查询参数
 func (c *condition) SQLValues() []interface{} {
 	return c.values
 }
