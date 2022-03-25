72fb8834cc3bd38e418a5249cff7a26105b89f08
toven <ttw130@gmail.com>
2022-03-25 16:19:55 +0800

    SQL 条件构造对象支持 and/or 参数

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/spf13/cast v1.4.1
-	github.com/zeromicro/go-zero v1.3.0
+	github.com/zeromicro/go-zero v1.3.1
 )
--- sql/query/condition.go
+++ sql/query/condition.go
@@ -10,19 +10,20 @@ type condition struct {
 	values []interface{}
 }
 
-// NewCondition 返回 SQL 条件构造对象
+// 返回 SQL 条件构造对象。
+// 同时只支持一种条件 and/or 判断，如果需要 and/or 拼接，可以创建两个不同的条件对象。
 func NewCondition() *condition {
 	return &condition{}
 }
 
-// Add 添加一个查询参数和值
+// 添加一个查询参数和值
 func (c *condition) Add(param string, value interface{}) {
 	c.params = append(c.params, param)
 	c.values = append(c.values, value)
 }
 
-// SQLParams 返回 SQL 查询参数字符串
-func (c *condition) SQLParams() string {
+// 返回 and 查询条件
+func (c *condition) AndParams() string {
 	if len(c.params) == 0 {
 		return ""
 	}
@@ -30,16 +31,25 @@ func (c *condition) SQLParams() string {
 	return strings.Join(c.params, " and ")
 }
 
-// SQLWhere 返回有 where 字符串的查询参数字符串
+// 返回 or 查询条件
+func (c *condition) OrParams() string {
+	if len(c.params) == 0 {
+		return ""
+	}
+
+	return strings.Join(c.params, " or ")
+}
+
+// 返回有 where 字符串的查询参数字符串
 func (c *condition) SQLWhere() string {
-	var s = c.SQLParams()
+	var s = c.AndParams()
 	if len(s) == 0 {
 		return s
 	}
 	return fmt.Sprintf(" where %s", s)
 }
 
-// SQLValues 返回 SQL 查询参数
+// 返回 SQL 查询参数
 func (c *condition) SQLValues() []interface{} {
 	return c.values
 }
